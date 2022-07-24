dc73805302e4fca539884876c8a4e01bb636928d
fredkeemhaus <fredkeemhaus@gmail.com>
2022-07-24 23:39:41 +0900

    same board movement

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -4,12 +4,18 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   padding: 20px 10px;
-  padding-top: 30px; 
+  padding-top: 10px; 
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
-  min-height: 200px;
+  min-height: 300px;
 `
 
+const Title = styled.div`
+    text-align: center;
+    font-weight: 600;
+    margin-bottom: 0px;
+    font-size: 18px;
+`
 
 interface IBoardProps {
     toDos: string[];
@@ -18,16 +24,19 @@ interface IBoardProps {
 
 function Board({toDos, boardId}: IBoardProps) {
     return (
-        <Droppable droppableId={boardId}>
-            {(magic) => (
-                <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-                    {toDos.map((toDo, index) => (
-                        <DraggableCard key={toDo} toDo={toDo} index={index} />
-                    ))}
-                    {magic.placeholder}
-                </Wrapper>
-            )}
-        </Droppable>
+        <Wrapper>
+            <Title>{boardId}</Title>
+            <Droppable droppableId={boardId}>
+                {(magic) => (
+                    <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
+                        {toDos.map((toDo, index) => (
+                            <DraggableCard key={toDo} toDo={toDo} index={index} />
+                        ))}
+                        {magic.placeholder}
+                    </Wrapper>
+                )}
+            </Droppable>
+        </Wrapper>
     )
 }
 
